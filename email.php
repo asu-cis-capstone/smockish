d9dda6069f235fb622153baa43b1a1da71a24326
Twayne <tado1@asu.edu>
2014-10-02 23:07:10 -0700

    Release v0.4

--- email.php
+++ email.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <?php
-$email = $_POST["email"];
-$fh = fopen("email.txt", "a");
-fwrite($fh, $email."\n");
-fclose($fh);
+	$host = 'localhost';
+	$user = 'tado1';
+	$pw = 'cis425';
+	$db = 'tado1';
+	
+	$dbc = mysqli_connect($host, $user, $pw, $db)
+	or die('Unable to connect to DB! Process aborted...');
+
+	$email = $_POST['email'];
+	
+	$query = "INSERT INTO smockishemail(email)" . 
+	"VALUES('$email')";
+	
+	$result = mysqli_query($dbc, $query)
+	or die('Unable to write to DB! Process aborted...');
+
+	mysqli_close($dbc);
 ?>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>PHP Testing Page</title>
+	<title>Email Testing Page</title>
 </head>
 <body> 
-<div><p style="font-size:500%; text-align:center;">TEST SUCCESS!</p></div>
+<div><p style="font-size:500%; text-align:center;">E-mail successfully entered into database!</p></div>
 </body>
 </html>
